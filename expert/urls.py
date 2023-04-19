e8cfdff714eb3988c8b6d4c9f75a89015784c7cb
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-04-19 21:41:03 +0200

    Add card and slider

--- expert/urls.py
+++ expert/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from expert.views import view_index
+from expert.views import view_index, view_card
 
 app_name = 'expert'
 
 urlpatterns = [
-    path('', view_index, name='index')
+    path('', view_index, name='index'),
+    path('card/', view_card, name='card')
 ]
--- expert/views.py
+++ expert/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Order
 
 # Create your views here.
 def view_index(request):
     return render(request, "geoexpert/index.html")
+
+
+def view_card(request):
+    order = Order.objects.all().first()
+
+    return render(request, 'geoexpert/card.html', context={'order': order})
