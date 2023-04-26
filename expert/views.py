d91d59428e4ac45aaa5f3a47fa7b28f7070614ab
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2023-04-26 08:35:04 +0200

    Add hints

--- expert/views.py
+++ expert/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Order, OrderImages
+from .models import Order
+from django.http import HttpRequest, HttpResponse
 from rest_framework import generics
 from .serializers import OrderSelializer
 from .permissions import IsOwnerOrReadOnly
@@ -11,13 +12,13 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsOwnerOrReadOnly,)
 
 
-def view_index(request):
+def view_index(request: HttpRequest) -> HttpResponse:
     objects = Order.objects.all()
 
     return render(request, "geoexpert/index.html", context={'objects': objects})
 
 
-def view_card(request):
+def view_card(request: HttpRequest) -> HttpResponse:
     order = Order.objects.all().first()
 
     return render(request, 'geoexpert/card.html', context={'order': order})
