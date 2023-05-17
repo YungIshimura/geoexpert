4c706f8be0b75b9a9c438a5d5ad69a491a2cbbdc
Kirillo1 <kirill.han011@gmail.com>
2023-05-17 15:07:59 +0300

    add and edit user avatar

--- users/forms.py
+++ users/forms.py
@@ -39,3 +39,9 @@ class UserProfileForm(UserChangeForm):
         super(UserProfileForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
+
+class AvatarUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('image',)
